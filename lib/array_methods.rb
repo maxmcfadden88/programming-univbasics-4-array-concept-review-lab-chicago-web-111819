0b53607009da4ed709f4c4672b0ea2d0e1557b6c
Max M <maxmcfadden88@gmail.com>
2019-11-10 23:08:02 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ puts array.index(element)
 end
+find_element_index(scale,2)
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
+  puts array.max
 end
+find_max_value(scale)
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
+  puts array.min
 end
+find_min_value(zig_zag)
